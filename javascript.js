7280eec2bfdefe53da238a7626a52e8f72682f81
rahulyogi6376 <deepakyogi6376@gmail.com>
2024-06-26 11:06:09 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let myName = "Rahul"
+let myLastName = "Yogi"
+let age = 22
+let isRahul = true;
+let deatils = {
+    firstName: "tushar",
+    age: 18,
+    class: "12th",
+    city: "jaipur",
+}
+console.log(deatils.age)
+// let fruits=['Apple','Orange','Banana']
+// document.write(myName)
+// document.write('<br>'+age)
+// document.write('<br>'+myName+" "+myLastName+" "+age)
+// document.write('<br>'+typeof age)
+// document.write('<br>'+typeof isRahul)
+// document.write('<br>'+isRahul)
+// console.log('<br>'+student)
+// document.write('<br>'+fruits)
